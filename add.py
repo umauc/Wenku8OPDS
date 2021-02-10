bbb122324a53dc625748dc3dd73291a0aa20e283
0x7FFFFF <w13657804853@163.com>
2021-02-10 17:14:11 +0800

    实现需求功能

--- add.py
+++ add.py
@@ -1,37 +1,30 @@
 import json
-from multiprocessing.dummy import freeze_support
 
 from local_settings import WENKU8_PASSWD, WENKU8_USERNAME
+
 from src.novel import Novel
 from src.user import SelfUser
-
 
 print("Adding Wenku8's Novels into data.json")
 print("It may take a long time...")
 
-novel_list = []
-novel_dict_list = []
-
 self_user = SelfUser(WENKU8_USERNAME, WENKU8_PASSWD)
 
 
-def add(i):
-    if __name__ == '__main__':
-        try:
-            if i != 0:
-                novel = Novel(i)
-                novel_list.append(novel)
-                novel_dict = {"book_path": f"https://wenku8.herokuapp.com/get/{novel.id}", "cover_path": novel.cover,
-                              "a_title": novel.title, "a_author": novel.author, "a_status": novel.status,
-                              "a_summary": novel.briefIntroduction, "tags": ""}
-                novel_dict_list.append(novel_dict)
-                return f"Added {novel.title}[{novel.id}]"
-        except ValueError:
-            pass
+def get_novel(number):
+    novel = Novel(number)
+    print(f"Added {novel.title}[{novel.id}]")
+    novel_dict = {"book_path": f"https://wenku8.herokuapp.com/get/{novel.id}", "cover_path": novel.cover,
+                  "a_title": novel.title, "a_author": novel.author, "a_status": novel.status,
+                  "a_summary": novel.briefIntroduction, "tags": ""}
+    return novel_dict
 
 
 if __name__ == '__main__':
-    freeze_support()
-    for i in range(3500):
-        add(i)
-    json.dump(novel_dict_list, open("data.json", "w+"))
+    novel_list = []
+    for i in range(1, 3000):
+        try:
+            novel_list.append(get_novel(i))
+        except AssertionError:
+            pass
+    json.dump(novel_list, open("data.json", "w+"))
--- src/novel.py
+++ src/novel.py
@@ -2,13 +2,16 @@ from typing import List
 
 import requests
 from bs4 import BeautifulSoup
-import copy
+from requests.exceptions import ConnectionError as CE
+from tenacity import retry, stop_after_attempt, retry_if_exception_type
+from urllib3.exceptions import ConnectionError, ProtocolError
 
 from src.user import SelfUser
 from src.utils import fastRegex
-from tenacity import retry, stop_after_attempt, retry_if_exception_type
-from urllib3.exceptions import ConnectionError, ProtocolError
-from requests.exceptions import ConnectionError as CE
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:62.0) Gecko/20100101 Firefox/62.0',
+}
 
 
 class Novel:
@@ -28,23 +31,23 @@ class Novel:
     @retry(stop=stop_after_attempt(3), retry=retry_if_exception_type(CE))
     @retry(stop=stop_after_attempt(3), retry=retry_if_exception_type(TimeoutError))
     def __init__(cls, articleid: int):
-        main_page = BeautifulSoup(
-            requests.get(f"http://www.wenku8.net/wap/article/articleinfo.php?id={articleid}").text,
-            features="html.parser")
+        main_page_request = requests.get(f"http://www.wenku8.net/book/{articleid}.htm", headers=headers,
+                                         cookies=SelfUser.cookies)
+        main_page_request.encoding = "gbk"
+        main_page = BeautifulSoup(main_page_request.text, features="html.parser")
         main_web_content = main_page.text
         cls.id = articleid
-        cls.title = fastRegex(r"新书\n(.*)", main_web_content)
-        cls.author = fastRegex(r"作者:(\w*)", main_web_content)
-        cls.library = fastRegex(r"类别:(\w*)", main_web_content)
-        cls.status = fastRegex(r"状态:(\w*)", main_web_content)
+        cls.title = fastRegex(r"板([\s\S]*)\[推", main_web_content).lstrip()
+        assert bool(cls.title)
+        cls.author = fastRegex(r"小说作者：(.*)", main_web_content)
+        cls.library = fastRegex(r"文库分类：(.*)", main_web_content)
+        cls.status = fastRegex(r"文章状态：(.*)", main_web_content)
         cls.copyright = True if main_web_content.find("版权问题") == -1 else False
-        if cls.copyright:
-            cls.totalWords = int(fastRegex(r"字数:(\d*)", main_web_content))
-        cls.briefIntroduction = fastRegex(r"\[作品简介\]([\s\S]*)联系管理员", main_web_content)
-        cls.cover = main_page.find("img")["src"]
+        cls.briefIntroduction = fastRegex(r"内容简介：([\s\S]*)阅读", main_web_content).lstrip().rstrip()
+        cls.cover = f"https://img.wenku8.com/image/{2 if cls.status == '连载中' else 0}/{cls.id}/{cls.id}s.jpg"
         read_page_request = requests.get(
-            f"https://www.wenku8.net/novel/{2 if cls.status == '连载中' else 0}/{articleid}/index.htm",
-            cookies=SelfUser.cookies)
+            f"http://www.wenku8.net/novel/{2 if cls.status == '连载中' else 0}/{articleid}/index.htm",
+            cookies=SelfUser.cookies, headers=headers)
         read_page_request.encoding = "gbk"
         read_page = BeautifulSoup(read_page_request.text,
                                   features="html.parser")
@@ -54,6 +57,6 @@ class Novel:
             if i["class"][0] == "vcss":
                 volumeList.append({"name": str(i.string), "chapters": []})
             elif i["class"][0] == "ccss" and i.string != "\xa0":
-                volumeList[len(volumeList) - 1]["chapters"].append({"name": str(i.string), "cid": int(i.a["href"].replace(".htm", ""))})
+                volumeList[len(volumeList) - 1]["chapters"].append(
+                    {"name": str(i.string), "cid": int(i.a["href"].replace(".htm", ""))})
         cls.volumeList = volumeList
-
